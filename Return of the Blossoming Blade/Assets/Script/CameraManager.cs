11a2d166c25ba2e4162351d4085bb79162983910
최희주 <gmlwnchl@naver.com>
2023-10-24 16:02:44 +0900

    add : out or mainland -ing

--- Return of the Blossoming Blade/Assets/Script/CameraManager.cs
+++ Return of the Blossoming Blade/Assets/Script/CameraManager.cs
@@ -9,10 +9,20 @@ public class CameraManager : MonoBehaviour
     public GameObject target;
     public float moveSpeed;
     private Vector3 targetPosition;
-    // Start is called before the first frame update
-    void Start()
+
+    public BoxCollider2D bound;
+
+    private Vector3 minBound;
+    private Vector3 maxBound;
+
+    private float halfWidth;
+    private float halfHeight;
+
+    private Camera theCamera;
+
+    private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             DontDestroyOnLoad(this.gameObject);
             instance = this;
@@ -23,6 +33,15 @@ public class CameraManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        theCamera = GetComponent<Camera>();
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -30,6 +49,17 @@ public class CameraManager : MonoBehaviour
         {
             targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+            float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+
+            this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
         }
+    }
+
+    public void SetBound(BoxCollider2D newBound)
+    {
+        bound = newBound;
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
     }
 }
--- Return of the Blossoming Blade/Assets/Script/TransferScene.cs
+++ Return of the Blossoming Blade/Assets/Script/TransferScene.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TransferMap : MonoBehaviour
+public class TransferScene : MonoBehaviour
 {
     public string transferMapName;
-
-    public Transform target;
-    public BoxCollider2D targetBound;
 
     private MovingObject thePlayer;
     private CameraManager theCamera;
@@ -26,9 +23,7 @@ public class TransferMap : MonoBehaviour
         if(collision.gameObject.name == "Player")
         {
             thePlayer.currentMapName = transferMapName;
-            theCamera.SetBound(targetBound);
-            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
-            thePlayer.transform.position = target.transform.position;
+            SceneManager.LoadScene(transferMapName);
         }
     }
 }
