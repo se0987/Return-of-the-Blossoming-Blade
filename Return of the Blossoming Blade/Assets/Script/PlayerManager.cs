ebc7a3e4bb304f1fa821d831fd54582f06ae66aa
DESKTOP-DOGVPUB\SSAFY <bejolly@naver.com>
2023-11-03 13:59:04 +0900

    update : skill logic

--- Return of the Blossoming Blade/Assets/Script/PlayerManager.cs
+++ Return of the Blossoming Blade/Assets/Script/PlayerManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerManager : MovingObject
 {
-    static public PlayerManager instance;
+    public static PlayerManager instance;
 
     public string currentMapName;
 
