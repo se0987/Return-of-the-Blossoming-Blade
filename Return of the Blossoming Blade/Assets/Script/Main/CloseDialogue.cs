688d8f752d689701525661a4d8c5acae7b1f2be2
최희주 <gmlwnchl@naver.com>
2023-11-07 17:19:54 +0900

    update : main option ending clear

--- Return of the Blossoming Blade/Assets/Script/Main/CloseDialogue.cs
+++ Return of the Blossoming Blade/Assets/Script/Main/CloseDialogue.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloseEnding : MonoBehaviour
+public class CloseDialogue : MonoBehaviour
 {
 
+    private ContinueDialogueManager continueDialogueManager;
     private EndingDialogueManager endingDialogueManager;
 
     void Start()
     {
+        continueDialogueManager = FindObjectOfType<ContinueDialogueManager>();
         endingDialogueManager = FindObjectOfType<EndingDialogueManager>();
     }
 
     // Start is called before the first frame update
     public void OnBtnClick()
     {
+        continueDialogueManager.CloseDialogue();
         endingDialogueManager.CloseDialogue();
     }
 }
