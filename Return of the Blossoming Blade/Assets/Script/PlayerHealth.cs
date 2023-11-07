8b6a0504e08478e1e45c04c1e7a19be38f9df304
정준우 <woojunjung18@gmail.com>
2023-11-07 09:01:34 +0900

    Feat:h '#' will be ignored, and an empty message aborts the commit.

---
+++ Return of the Blossoming Blade/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("³¡!");
+    }
+}
