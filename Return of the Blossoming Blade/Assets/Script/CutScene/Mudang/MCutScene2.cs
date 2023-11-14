da4ebdcf5fcfff03d99ee1c8a662ad5e7c8897b7
최희주 <gmlwnchl@naver.com>
2023-11-14 15:14:00 +0900

    fix : scene 3, scene 1

--- Return of the Blossoming Blade/Assets/Script/CutScene/Mudang/MCutScene2.cs
+++ Return of the Blossoming Blade/Assets/Script/CutScene/Mudang/MCutScene2.cs
@@ -11,6 +11,7 @@ public class MCutScene2 : MonoBehaviour
     private OrderManager theOrder;
     private PlayerManager thePlayer;
     private ChoiceManager theChoice;
+    private BGMManager theBGM;
 
     //private bool flag;
     private bool can = false;
@@ -23,6 +24,7 @@ public class MCutScene2 : MonoBehaviour
         theOrder = FindObjectOfType<OrderManager>();
         thePlayer = FindObjectOfType<PlayerManager>();
         theChoice = FindObjectOfType<ChoiceManager>();
+        theBGM = FindObjectOfType<BGMManager>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -52,6 +54,7 @@ public class MCutScene2 : MonoBehaviour
         theOrder.Move("Player", "LEFT");
 
         theDM.ShowDialogue(dialogue_1);
+        theBGM.Play(1);
         yield return new WaitUntil(() => !theDM.talking);
         //청명이 장로 때리기 가능? 불가능
         theDM.ShowDialogue(dialogue_2);
