1f1c777919f106fb708a711f0269ba51f3d70d0a
정준우 <woojunjung18@gmail.com>
2023-11-16 02:46:42 +0900

    Feat: 천마 패턴

---
+++ Return of the Blossoming Blade/Assets/Script/Raining.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raining : MonoBehaviour
+{
+    private bool hasCollided = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!hasCollided && other.gameObject.CompareTag("Player"))
+        {
+
+            PlayerStatus playerStatus = other.GetComponent<PlayerStatus>();
+            if (playerStatus != null)
+            {
+                playerStatus.TakeDamage(40.0f);
+                hasCollided = true;
+            }
+        }
+    }
+}
