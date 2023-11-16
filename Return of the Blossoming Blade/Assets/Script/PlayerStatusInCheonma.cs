21f398345976a2d792208802efe0b4c4831a1b82
정준우 <woojunjung18@gmail.com>
2023-11-16 12:27:34 +0900

    Feat: 천마 전투에서만 청명 사망 시 엔딩으로 이어지도록 구현

--- Return of the Blossoming Blade/Assets/Script/PlayerStatusInCheonma.cs
+++ Return of the Blossoming Blade/Assets/Script/PlayerStatusInCheonma.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class PlayerStatus : MonoBehaviour
+public class PlayerStatusInCheonma : MonoBehaviour
 {
     public float maxHP = 100f; // 최대 HP
     public float currentHP;
@@ -67,32 +66,18 @@ public class PlayerStatus : MonoBehaviour
     // 데미지를 입었을 때 호출되는 함수
     public void TakeDamage(float damage)
     {
-        // 체력 감소
-        currentHP -= damage;
 
-        // 체력이 0 이하가 되었는지 확인
-        if (currentHP <= 0)
+        // HP가 0 미만이면 0으로 설정
+        if (PlayerPrefs.GetFloat("playerHP") - damage < 0f)
         {
-            currentHP = 0; // 체력을 0으로 설정
-            PlayerPrefs.SetFloat("playerHP", currentHP);
-
-            // 현재 씬 이름 확인
-            string currentSceneName = SceneManager.GetActiveScene().name;
-
-            // 'Chunma' 씬인 경우 DieInCheonma 호출
-            if (currentSceneName == "Chunma")
-            {
-                DieInCheonma();
-            }
-
-            StartCoroutine(FlashCoroutine()); // 피해 입은 효과
+            PlayerPrefs.SetFloat("playerHP", 0);
+            currentHP = 0;
+            // TODO: 플레이어 사망 로직 구현
         }
-        else
-        {
-            // 체력이 0보다 큰 경우에만 PlayerPrefs에 저장
-            PlayerPrefs.SetFloat("playerHP", currentHP);
-            StartCoroutine(FlashCoroutine()); // 피해 입은 효과
-        }
+        currentHP = PlayerPrefs.GetFloat("playerHP") - damage;
+        PlayerPrefs.SetFloat("playerHP", currentHP);
+
+        StartCoroutine(FlashCoroutine()); // 피해 입은 효과
     }
     private IEnumerator FlashCoroutine()
     {
@@ -179,65 +164,6 @@ public class PlayerStatus : MonoBehaviour
                 playerMP = PlayerPrefs.GetFloat("playerMP");
 
                 currentMP = playerMP;
-            }
-        }
-    }
-
-    void DieInCheonma()
-    {
-        End5.end = true;
-        TransferMap[] temp = FindObjectsOfType<TransferMap>();
-
-        GameObject Cheonma = GameObject.Find("Cheonma Bon In");
-
-        DisableCheonmaBehaviors(Cheonma);
-        ResetCheonmaAnimator(Cheonma);
-        DisableAllClonedSpriteRenderers();
-
-        for (int i = 0; i < temp.Length; i++)
-        {
-            if (temp[i].gateName.Equals("EndPoint5"))
-            {
-                temp[i].GoToPoint();
-                break;
-            }
-        }
-        PlayerPrefs.SetFloat("End5", 1);
-    }
-
-    void DisableCheonmaBehaviors(GameObject cheonma)
-    {
-        foreach (var behavior in cheonma.GetComponents<Behaviour>())
-        {
-            behavior.enabled = false;
-        }
-
-        Animator animator = cheonma.GetComponent<Animator>();
-        if (animator != null)
-        {
-            animator.enabled = false;
-        }
-    }
-
-    void ResetCheonmaAnimator(GameObject cheonma)
-    {
-        Animator animator = cheonma.GetComponent<Animator>();
-        if (animator != null)
-        {
-            animator.Rebind();
-            animator.enabled = false;
-        }
-    }
-
-    void DisableAllClonedSpriteRenderers()
-    {
-        SpriteRenderer[] allSpriteRenderers = FindObjectsOfType<SpriteRenderer>();
-
-        foreach (SpriteRenderer renderer in allSpriteRenderers)
-        {
-            if (renderer.gameObject.name.Contains("(Clone)"))
-            {
-                renderer.enabled = false;
             }
         }
     }
