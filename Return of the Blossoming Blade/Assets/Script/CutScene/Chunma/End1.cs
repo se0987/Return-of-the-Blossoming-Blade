65903eabec337a342cb836e8355ca7dbd4b4f9fd
정준우 <woojunjung18@gmail.com>
2023-11-16 11:53:26 +0900

    Feat: 천마 엔딩 연결

--- Return of the Blossoming Blade/Assets/Script/CutScene/Chunma/End1.cs
+++ Return of the Blossoming Blade/Assets/Script/CutScene/Chunma/End1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class End1 : MonoBehaviour
 {
@@ -27,7 +28,7 @@ public class End1 : MonoBehaviour
     private bool can = false;
     private bool one = true;
 
-    public bool end = false;
+    public static bool end = false;
     private bool stop = false;
 
     // Start is called before the first frame update
@@ -88,8 +89,23 @@ public class End1 : MonoBehaviour
 
         theDM.ShowDialogue(dialogue_2);
         theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
         theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
         theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
+        theOrder.Move("Player", "UP");
+        yield return new WaitForSeconds(0.2f);
         theOrder.Move("Player", "UP");
         yield return new WaitUntil(() => !theDM.talking);
         if (stop)
@@ -104,6 +120,21 @@ public class End1 : MonoBehaviour
             yield break;
         }
         theOrder.Action("Player", "AttackH");
+
+        GameObject Cheonma = GameObject.Find("Cheonma Bon In");
+        GameObject bossHpBarObject = GameObject.Find("Boss_HP_Gauge1");
+        if (bossHpBarObject != null)
+        {
+            Image bossHpBarImage = bossHpBarObject.GetComponent<Image>();
+            if (bossHpBarImage != null)
+            {
+                bossHpBarImage.fillAmount = 0;
+            }
+        }
+        if (Cheonma != null)
+        {
+            Cheonma.SetActive(false);
+        }
 
         bgmManager.Stop();
         theAudio.Play(swordSound);
